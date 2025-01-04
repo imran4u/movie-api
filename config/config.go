8c582c23fee19bdba300838c84e7c10a597fefb1
imran4u <er.imran4u@gmail.com>
2025-01-04 16:23:36 +0530

    minor update

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package config
 
 const (
+	// Use your connection string, get it from mongodb atlas site https://cloud.mongodb.com/
 	ConnectionString = "mongodb+srv://erimran4u:imrantest123@movieclustor0.eumdv.mongodb.net/?retryWrites=true&w=majority&appName=movieclustor0"
 	DbName           = "netflix"
 	ColName          = "watchlist"
